bfcc0239ea17ba882a004635372c7a5f634aabbf
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-03-14 10:36:32 +0700

    create load saved list captain function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,13 +4,29 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 // Generate random index
 const randomIndex = (length) => Math.floor(Math.random() * length);
+const listNameEl = document.getElementById('listNames');
 
 let newCaptain = "";
+
+const LoadSavedListCaptain = () => {
+    const captain = localStorage.getItem("captain");
+    if (captain === null) {
+        newCaptain = "";
+    } else {
+        listNameEl.textContent = JSON.parse(captain)
+    }
+}
+
+LoadSavedListCaptain()
+
+listNameEl.addEventListener("change", () => {
+    newCaptain = JSON.stringify(listNameEl.value);
+    localStorage.setItem("captain", newCaptain);
+})
 
 // Event listener pick captain button
 const pickACaptainButton = document.getElementById('pickACaptainButton')
 pickACaptainButton.addEventListener("click", (e) => {
-    const listNameEl = document.getElementById('listNames');
     const newCaptainEl = document.getElementById('newCaptain');
     const names = listNameEl.value.trim().split('\n');
 
