cf292e600531ff0b20e71a1029e83613d584fa36
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-03-14 10:36:43 +0700

    reformat

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,44 +1,42 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-    e.preventDefault()
+  e.preventDefault()
 })
 
 // Generate random index
-const randomIndex = (length) => Math.floor(Math.random() * length);
-const listNameEl = document.getElementById('listNames');
+const randomIndex = (length) => Math.floor(Math.random() * length)
+const listNameEl = document.getElementById("listNames")
 
-let newCaptain = "";
+let newCaptain = ""
 
 const LoadSavedListCaptain = () => {
-    const captain = localStorage.getItem("captain");
-    if (captain === null) {
-        newCaptain = "";
-    } else {
-        listNameEl.textContent = JSON.parse(captain)
-    }
+  const captain = localStorage.getItem("captain")
+  if (captain === null) {
+    newCaptain = ""
+  } else {
+    listNameEl.textContent = JSON.parse(captain)
+  }
 }
 
 LoadSavedListCaptain()
 
 listNameEl.addEventListener("change", () => {
-    newCaptain = JSON.stringify(listNameEl.value);
-    localStorage.setItem("captain", newCaptain);
+  newCaptain = JSON.stringify(listNameEl.value)
+  localStorage.setItem("captain", newCaptain)
 })
 
 // Event listener pick captain button
-const pickACaptainButton = document.getElementById('pickACaptainButton')
+const pickACaptainButton = document.getElementById("pickACaptainButton")
 pickACaptainButton.addEventListener("click", (e) => {
-    const newCaptainEl = document.getElementById('newCaptain');
-    const names = listNameEl.value.trim().split('\n');
+  const newCaptainEl = document.getElementById("newCaptain")
+  const names = listNameEl.value.trim().split("\n")
 
-    newCaptain = names[randomIndex(names.length)]
+  newCaptain = names[randomIndex(names.length)]
 
-    // Clear box value before append a new one
-    newCaptainEl.value = ''
+  // Clear box value before append a new one
+  newCaptainEl.value = ""
 
-    // Append winner
-    setTimeout(() => {
-        newCaptainEl.value = newCaptain
-    }, 1000)
+  // Append winner
+  setTimeout(() => {
+    newCaptainEl.value = newCaptain
+  }, 1000)
 })
-
-
